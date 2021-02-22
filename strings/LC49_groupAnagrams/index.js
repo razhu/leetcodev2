8c283954abe7219fcb9d557dd8aa20b1e29ea554
ramiro <rhuayller@agetic.gob.bo>
2021-02-22 15:42:14 -0400

    avane

--- strings/LC49_groupAnagrams/index.js
+++ strings/LC49_groupAnagrams/index.js
@@ -1,3 +1,21 @@
-function groupAnagrams(strs) {}
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+function groupAnagrams(strs) {
+    let sw = {}
+    for (let i = 0; i < strs.length; i++) {
+        const value = strs[i];
+        const ordered = value.split('').sort().join('')
+        if (!sw[ordered]) {
+            sw[ordered] = []
+        }
+        sw[ordered].push(value)
+        
+    }
+    return Object.values(sw);
+}
 
 module.exports = groupAnagrams;
+
+
+s.split('').reverse().join('')
