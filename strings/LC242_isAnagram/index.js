c72614097d39408ae94d5e355bd4008a57bf1bea
ramiro <rhuayller@agetic.gob.bo>
2021-02-23 08:44:13 -0400

    valid anagram

--- strings/LC242_isAnagram/index.js
+++ strings/LC242_isAnagram/index.js
@@ -1,3 +1,19 @@
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+    if (s.length !== t.length) return false
+    let sw = {}
+    for (let i = 0; i < s.length; i++) {
+        const e = s[i];
+        sw[e] = sw[e] + 1 || 1
+    }
+    for (let j = 0; j < t.length; j++) {
+        const e2 = t[j];
+        if (!sw[e2]) {
+            return false
+        } else {
+            sw[e2]--
+        }
+    }
+    return true
+}
 
 module.exports = isAnagram;
