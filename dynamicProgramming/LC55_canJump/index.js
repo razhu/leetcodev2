3a6072fc0c5cf75782272d8434d9023465592d78
ramiro <rhuayller@agetic.gob.bo>
2021-02-23 09:06:26 -0400

    avance

--- dynamicProgramming/LC55_canJump/index.js
+++ dynamicProgramming/LC55_canJump/index.js
@@ -1,3 +1,16 @@
-function canJump(nums) {}
+function canJump(nums) {
+    let dpArray = []
+    nums.map(e => dpArray.push(false))
+    dpArray[0] = true
+    let left = 0
+    let right = 1
+    while (left < right) {
+        if (nums[dpArray[left] !== nums[right]]) return false
+        left = nums[left]
+        right = nums[left]
+    }
+    return true
+}
 
+// console.log(canJump([1, 2, 3]));
 module.exports = canJump;
