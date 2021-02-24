73d3d97f849782ac678d7c88436e56aa5ebf2f74
ramiro <rhuayller@agetic.gob.bo>
2021-02-24 12:19:23 -0400

    lc55

--- dynamicProgramming/LC55_canJump/index.js
+++ dynamicProgramming/LC55_canJump/index.js
@@ -1,15 +1,27 @@
 function canJump(nums) {
-    let dpArray = []
-    nums.map(e => dpArray.push(false))
-    dpArray[0] = true
-    let left = 0
-    let right = 1
-    while (left < right) {
-        if (nums[dpArray[left] !== nums[right]]) return false
-        left = nums[left]
-        right = nums[left]
+    // let dpArray = []
+    // nums.map(e => dpArray.push(false))
+    // dpArray[0] = true
+    // let left = 0
+    // let right = 1
+    // while (left < right) {
+    //     if (nums[dpArray[left] !== nums[right]]) return false
+    //     left = nums[left]
+    //     right = nums[left]
+    // }
+    // return true
+let dpArr = nums.map(n => false)
+dpArr[0] = true
+
+for (let i = 1; i < nums.length; i++) { // will loop to update dpArr
+    for (let j = 0; j < i; j++) {
+        if (nums[j] && j + nums[j] >= i) {
+            dpArr[i] = true
+            break
+        }
     }
-    return true
+    }
+    return dpArr[dpArr.length - 1]
 }
 
 // console.log(canJump([1, 2, 3]));
