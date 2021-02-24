2da888d6132f05f17d516c2bca6cd1e793c2003f
ramiro <rhuayller@agetic.gob.bo>
2021-02-24 16:03:51 -0400

    learn

--- learn.js
+++ learn.js
@@ -18,9 +18,8 @@
 // starting point finish point
 
 function travel (m, n, memo = {}) {
-    const key = m + ',' + 'n'
+    const key = m + ',' + n
     if (key in memo) return memo[key]
-    // if (memo[m][n]) return memo[m][n] 
     if (m === 1 && n === 1) return 1
     if (m === 0 || n === 0) return 0
     memo[key] = travel(m-1, n, memo) + travel(m, n-1, memo)
