74a66a3b30b5b958747f8ce25d882462f3071f72
ramiro <ramihertz@gmail.com>
2021-03-01 22:25:18 -0400

    ok

--- learn/learn.js
+++ learn/learn.js
@@ -6,7 +6,7 @@
 // }
 
 // best with memoization aka caching
-// function fibo (n, memo = {}) {
+// function fibo (n, memo = {}) { 
 //     if (n in memo) return memo[n]
 //      if (n <= 2) return 1
 //      memo[n] = fibo(n - 1, memo) + fibo(n - 2, memo)
