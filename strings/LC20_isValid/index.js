0569e5d69d2c68f9017b96fc201194ac6b23993e
ramiro <rhuayller@agetic.gob.bo>
2021-03-08 15:52:26 -0400

    ok

--- strings/LC20_isValid/index.js
+++ strings/LC20_isValid/index.js
@@ -1,3 +1,23 @@
-function isValid(s) {}
+function isValid(s) {
+    // method to use: stack
+    let respuesta = false
+    const simbolos = {
+        '{': '}',
+        '[': ']',
+        '(': ')',
+    }
+    let miStack = []
+    for (let i = 0; i < s.length; i++) {
+        const e = s[i];
+        if (!simbolos[e]) { // no existe
+            if (simbolos[e] === e) {
+                return false
+            }
+        }
+        
+    }
+
+    return true
+}
 
 module.exports = isValid;
