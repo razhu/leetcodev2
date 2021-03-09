9a83ad814f0a1e7796d591698bb5699e495dd804
ramiro <rhuayller@agetic.gob.bo>
2021-03-09 13:03:41 -0400

    palindrome

--- strings/LC125_isPalindrome/index.js
+++ strings/LC125_isPalindrome/index.js
@@ -1,13 +1,6 @@
 function isPalindrome(s) {
-    s = s.replace(/[^0-9a-z]/gi, '').toLocaleLowerCase()
-    let left = 0;
-    let right = s.length - 1
-    while (left < right) {
-        if (s[left] !== s[right]) return false
-        left++
-        right--
-    }
-    return true
+    let t = s.replace(/[^0-9a-z]/gi, '').toLocaleLowerCase()
+    return t.split('').reverse().join('') === t
 }
 
 
